2e2b24b04ea2c827568a61cdcef7306d9a4758d0
rabiasohail098 <rabiasohail1209@gmail.com>
2024-07-12 07:34:02 +0500

    step 02

---
+++ main.ts
@@ -0,0 +1,11 @@
+// step 02 const and let
+// we use let when we reassigne our value because let allow to reassigne the value but
+//not allow to redeclaration
+let myName:string = "Rabia Gohar";
+myName = "Yashal khan";
+
+// we used const when our value is constant because const not allowed to reassigne the value
+// and redeclaration
+
+const rollNo:number = 4553276;
+rollNo = 7876987; // show error because it is constant
